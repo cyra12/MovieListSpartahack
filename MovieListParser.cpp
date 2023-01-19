ea97c6bef44d5678c72dfc8c6c66bf3cdd0c04cb
cyra12 <103450225+cyra12@users.noreply.github.com>
2023-01-19 17:28:38 -0500

    CLI works kinda
    checks for a ; in the string passed into it, if its there it adds that string to the movie text file, if not it searches the text file for movies with that genre in its description

--- MovieListParser.cpp
+++ MovieListParser.cpp
@@ -2,14 +2,12 @@
 #include <fstream>
 #include <string>
 #include <regex>
-#include <vector>
-using std::vector;
 using std::string;
 using std::fstream;
 
 string const delimiter = ";";
 //searches the Movies text file for movies with the genre specified
-void FindMovieOfGenre(string const & genre, vector<string> & listOfFoundMovies) {
+void FindMoviesOfGenre(string const & genre, vector<string> & listOfFoundMovies) {
     fstream newfile;
     newfile.open("Movies.txt", std::ios::in);
     string currentMovie;
@@ -25,31 +23,9 @@ void FindMovieOfGenre(string const & genre, vector<string> & listOfFoundMovies) 
     newfile.close();
 }
 
-
-//appends genres to the end of the movie title using the proper behavior for the rest of the project.
-void GenreAdderOneAtATime(string & MovieTitle) {
-    //for now, we will get genre information by taking cin.
-    string genre;
-    MovieTitle += delimiter + ",";
-    while(getline(std::cin, genre) && genre != "0") {
-        MovieTitle += " " + genre + ",";
-    }
-    MovieTitle += "\\n";
-}
-
-void GenreAdderAllFromStandardInput(string & MovieTitle) {
-    string GenreList;
-    getline(std::cin, GenreList);
-    MovieTitle += delimiter + GenreList;
-}
-
-
 //add movie to the text file of movies.
-//not quite sure yet how to get the genre information, thats something I'll worry about later.
 void AddMovieToList () {
-    //GenreAdderOneAtATime(MovieTitle);
 
-    //GenreAdderAllFromStandardInput(MovieTitle);
     string MovieTitle;
     getline(std::cin, MovieTitle);
 
@@ -59,26 +35,14 @@ void AddMovieToList () {
     MovieList.close();
 }
 
-
-
-
 int main () {
-    AddMovieToList();
-    //string movietitle = "Star Wars";
-    // getline(std::cin, movietitle);
-    //AddMovieToList(movietitle);
-    // std::cout << "Movie added" << std::endl;
-
-    // vector<string> foundMovies;
-    
-    // string genreForSearch;
-    // getline(std::cin, genreForSearch);
-    // FindMovieOfGenre(genreForSearch, foundMovies);
-    // for(string s: foundMovies) {
-    //     std::cout << s << std::endl;
-    // }
-
-
+    string command;
+    getline(std::cin, command);
+    if(command.find(";") != std::string::npos) {
+        AddMovieToList(command);
+    } else {
+        FindMoviesOfGenre(command);
+    }
 
     std::cout << std::endl << "program end reached" << std::endl;
     return 0;
